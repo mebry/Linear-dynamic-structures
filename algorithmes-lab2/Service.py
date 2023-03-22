bc82b06beb4cfdde170e5f5fdbfd845215c2703c
mebry <vadushko@gmail.com>
2023-03-22 21:07:58 +0300

    Update a service file.

--- algorithmes-lab2/Service.py
+++ algorithmes-lab2/Service.py
@@ -1,32 +1,85 @@
+from random import randint
 from LinkedList import LinkedList
+from DynamicArray import DynamicArray
 
 
-def search(key,lst):
-    current_node = lst.head
-    while current_node != None:
-        if current_node.data==key:
-            return True
-        current_node = current_node.next
-    return False
+def display_list(lst):
+    cur_node = lst.head
+    output = ""
+    while cur_node != None:
+            output += str(cur_node.data) + " "
+            cur_node = cur_node.next
+    print(output)    
 
-def calculate(head,list2,list3,resultList):
+
+
+def display_array(arr):
+    i = 0
+    output = ""
+    while i < arr.countn:
+        output += str(arr.data[i]) + " "
+        i += 1
+    print(output) 
+           
+
+def calculate_for_list(head,list2,list3,result_list):
     while head:
-        if (not search(head.data,list2)) and (not search(head.data,list3)):
-            resultList.add(head.data)
+        if (not list2.contains(head.data)) and not list3.contains(head.data):
+            result_list.add(head.data)
         head=head.next
+
+
+def calculate_for_array(arr1,arr2,arr3,result_arr):
+    for i in range(arr1.count): 
+        if (not arr2.contains(arr1.data[i]) and not arr3.contains(arr1.data[i])):
+            result_arr.add(arr1.data[i])
+            
 
 def unique_elements(list1,list2,list3):
 
     result_list=LinkedList()
 
-    calculate(list1.head,list2,list3,result_list);
-
-    calculate(list2.head,list1,list3,result_list);
-
-    calculate(list3.head,list1,list2,result_list);
+    calculate_for_list(list1.head,list2,list3,result_list)
+    calculate_for_list(list2.head,list1,list3,result_list)
+    calculate_for_list(list3.head,list1,list2,result_list)
    
     return result_list
 
+
+
+def unique_elements_for_arrays(arr1,arr2,arr3): 
+
+   result_arr=DynamicArray() 
+
+   calculate_for_array(arr1,arr2,arr3,result_arr) 
+   calculate_for_array(arr2,arr1,arr3,result_arr) 
+   calculate_for_array(arr3,arr1,arr2,result_arr) 
+
+   return result_arr
+
+def fill_list(count_elements, lst,min_value=-10,max_value=20):
+    i = 0
+    while(i < int(count_elements)):
+        lst.add(randint(min_value, max_value))
+        i += 1
+
+
+#def unique_elements_for_arrays(arr1,arr2,arr3): 
+#   result_arr=DynamicArray() 
+    
+#   for i in range(arr1.count): 
+#      if (not arr2.contains(arr1.data[i])) and (not arr3.contains(arr1.data[i])): 
+#          result_arr.append(arr1.data[i]) 
+            
+#   for i in range(arr2.count): 
+#      if (not arr1.contains(arr2.data[i])) and (not arr3.contains(arr2.data[i])): 
+#          result_arr.append(arr2.data[i]) 
+            
+#   for i in range(arr3.count): 
+#      if (not arr1.contains(arr3.data[i])) and (not arr2.contains(arr3.data[i])):  
+#          result_arr.append(arr3.data[i]) 
+            
+#   return result_arr 
 
 #def unique_elements(list1,list2,list3):
 #    result_list=LinkedList()
